b609c0d759de401035793998ddbbfba313bf6721
gustavoquincy <yuankaile@gmail.com>
2022-05-02 18:08:52 +0800

    using trait objects that allow for values of different types

--- src/lib.rs
+++ src/lib.rs
@@ -29,3 +29,31 @@ impl AveragedCollection {
         self.average = total as f64 / self.list.len() as f64;
     }
 }
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct  Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+    }
+}
